b536f5da483d8cab839223a74058f7d1039c9149
Supaplex777 <74548247+Supaplex777@users.noreply.github.com>
2021-03-11 11:26:11 +0300

    Создал тело бота,логирование (#1)
    * Создал тело бота,логирование
    
    * Изменил расположение импорт,исправил синтаксис,убрал logging info
    
    * выставил по алфавиту CommandHandler,Updater
    
    * убрал print и вернул logging.info
    
    * Изменил синтаксис

---
+++ bot.py
@@ -0,0 +1,25 @@
+import logging # общедоступная библиотека
+from telegram.ext import CommandHandler,Updater # часть общепотребимой библиотеки
+import settings # мой модуль 
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+def start_user(update,context):
+    update.message.reply_text(f'Здравствуй пользователь!')
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', start_user))
+
+    logging.info('Бот стартовал')
+
+
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == '__main__':
+    main()
+
