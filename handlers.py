5a4a5afd34ba7c0e8a2e8cbab528e2e70c4c11b5
Ksenia <eternidad@inbox.ru>
2021-03-12 16:09:15 +0300

    Added file texts.py for all text messages to users

--- handlers.py
+++ handlers.py
@@ -1,8 +1,6 @@
+from texts import welcome_text
 from utils import study_options_keybord
 
-
-welcome_text = '''Привет! Выбери направление, которое ты хотел бы изучить.
-Если тебе трудно определиться, нажми кнопку "Затрудняюсь..." и мы поподробней расскажем о каждом из направлений.'''
 
 def start_bot(update,context):
     update.message.reply_text(welcome_text, reply_markup=study_options_keybord())
