ba70b4a072f7c90dbf8008cab34e689e0f177bab
Eugen <cmrrus@rambler.ru>
2021-03-17 19:48:43 +0300

    Исправил имена, поменял __repr__, заполнил базу данных для теста

--- bd/add_user.py
+++ bd/add_user.py
@@ -1,12 +1,12 @@
 from db import db_session # добавление записи в базу данных Основная
-from models import Main
+from models import DifinitionOfSkill
 
-first=Main(direction=3, course_name='Курс Веб-аналитика', school_name=1, the_cost=35000, appraisal= 4.1,link='https://imba.ru/course/web-analytics?utm_medium=referral&utm_source=tutortop.ru&utm_campaign=partner')
+#first=Courses(direction=3, course_name='Курс Веб-аналитика', school_name=11, the_cost=38000, appraisal= 4.2,link='https://imba.ru/course/web-analytics?utm_medium=referral&utm_source=tutortop.ru&utm_campaign=partner')
 
-#second=direct(direction='Аналитика')
-#three=The_skill(skill='Машинное обучение')
-#four=The_Scholl(scholl='Convert Monster')
-#five = The_difinition_of_skill(course_name=1,skill=2) - Не работает 
+#second=Direction(flow='Аналитика')
+#three=Skill(knowledge='Машинное обучение') 
+#four=Scholl(scholl='Convert Monster')
+five =DifinitionOfSkill(course_name=1,skill=2) #Не работает 
 
 db_session.add(five)
 db_session.commit()
--- bd/models.py
+++ bd/models.py
@@ -2,51 +2,51 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from db import Base, engine
 
 
-class direct(Base):
-    __tablename__ = 'direction' # когда меняешь имя меняется и таблица
+class Direction(Base): # 2 . направление
+    __tablename__ = 'dire' # когда меняешь имя меняется и таблица
     id = Column(Integer, primary_key=True) # primary первичный ключ 
-    direction = Column(String) # направление
+    flow = Column(String) # течение
 
     def __repr__(self):
-        return f"User {self.name}, {self.the_cost}"
+        return f"User {self.flow}"
 
-class The_skill(Base):
-    __tablename__ = 'skill' # когда меняешь имя меняется и таблица
+class Skill(Base): #3
+    __tablename__ = 'experience' # (опыт) когда меняешь имя меняется и таблица
     id = Column(Integer, primary_key=True) # primary первичный ключ 
-    skill = Column(String) # навыки
+    knowledge = Column(String) # перевод знания
     
     def __repr__(self):
-        return f"User {self.name}, {self.the_cost}"
+        return f"User {self.knowledge}"
 
-class The_Scholl(Base):
-    __tablename__ = 'scholl'
+class Scholl(Base): #4
+    __tablename__ = 'studio'
     id = Column(Integer, primary_key=True) # primary первичный ключ 
-    scholl = Column(String) # навыки
+    scholl = Column(String) # школа
 
     def __repr__(self):
-        return f"User {self.name}, {self.the_cost}"
+        return f"User {self.school}"
 
-class The_difinition_of_skill(Base):
-    __tablename__ = 'difinition_of_skill'
+class DifinitionOfSkill(Base): #5
+    __tablename__ = 'skillratio' # соотношение навыков
     id = Column(Integer, primary_key=True)
-    course_name = Column(Integer,ForeignKey('the_main.id'), index=True, nullable=False)
-    skill = Column(Integer,ForeignKey('skill.id'), index=True, nullable=False)
+    course_name = Column(Integer,ForeignKey('coursetable.id'), index=True, nullable=False)
+    skill = Column(Integer,ForeignKey('experience.id'), index=True, nullable=False)
      
     def __repr__(self):
-        return f"User {self.name}, {self.the_cost}"
+        return f"User {self.course_name},{self.skill}"
 
-class Main(Base):
-    __tablename__ = 'the_main' # когда меняешь имя меняется и таблица
+class Courses(Base): #1
+    __tablename__ = 'coursetable' # когда меняешь имя меняется и таблица
     id = Column(Integer, primary_key=True) # primary первичный ключ 
-    direction = Column(Integer,ForeignKey('direction.id'), index=True, nullable=False) # направление
+    direction = Column(Integer,ForeignKey('dire.id'), index=True, nullable=False) # направление
     course_name = Column(String) #название курсов
-    school_name = Column(Integer,ForeignKey('scholl.id'), index=True, nullable=False) #имя школы
+    school_name = Column(Integer,ForeignKey('studio.id'), index=True, nullable=False) #имя школы
     the_cost = Column(Integer) # стоимость
     appraisal = Column(Float) # оценка
     link = Column(String(400), unique=True) # уникальна не может быть одинаковой ссылки
 
     def __repr__(self):
-        return f"User {self.name}, {self.the_cost}"
+        return f"User {self.direction}, {self.course_name},{self.school.name},{self.the_cost},{self.appraisal},{self.link}"
 
 
 if __name__ == "__main__":
