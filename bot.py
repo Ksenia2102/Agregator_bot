56cf94e6865450948044edce847ea3623ae2dcfe
Ksenia2102 <57745661+Ksenia2102@users.noreply.github.com>
2021-03-18 11:00:02 +0300

    added main dialog line (#4)

--- bot.py
+++ bot.py
@@ -1,28 +1,40 @@
 import logging
 
-from telegram import KeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import (CommandHandler, ConversationHandler, Filters,
+                          MessageHandler, Updater)
 
 import settings
-from handlers import start_bot
+from main_dialog import generate_courses, generate_skills, start_bot
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 PROXY = {
     'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': 
-        {'username': settings.PROXY_USERNAME, 
-        'password': settings.PROXY_PASSWORD}}
+    'urllib3_proxy_kwargs':
+        {'username': settings.PROXY_USERNAME,
+         'password': settings.PROXY_PASSWORD}
+        }
+
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
     dp = mybot.dispatcher
 
-    dp.add_handler(CommandHandler('start', start_bot))
+    # Основная линия диалога с пользователем
+    main_dialog = ConversationHandler(
+        entry_points=[CommandHandler('start', start_bot)],
+        states={
+            'skills': [MessageHandler(Filters.text, generate_skills)],
+            'courses': [MessageHandler(Filters.text, generate_courses)]
+        },
+        fallbacks=[]
+    )
+
+    dp.add_handler(main_dialog)
 
     mybot.start_polling()
     mybot.idle()
 
+
 if __name__ == '__main__':
     main()
-
--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
-from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
+
+from settings import SKILLS, STUDY_OPTIONS
+
 
 def study_options_keybord():
-    return ReplyKeyboardMarkup([['Программирование', 'Дизайн', 'Аналитика'], ['Затрудняюсь...']], one_time_keyboard=True, resize_keyboard=True)
+    return ReplyKeyboardMarkup(
+        [STUDY_OPTIONS, ['Затрудняюсь...']],
+        one_time_keyboard=True, resize_keyboard=True
+        )
+
+
+def skills_keyboard(study_option):
+    skills = SKILLS[study_option]
+    return ReplyKeyboardMarkup(
+        [skills], one_time_keyboard=True, resize_keyboard=True
+    )
